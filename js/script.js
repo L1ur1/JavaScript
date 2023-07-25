8eeb6bc8782b4098e2a9dd77913d5a812021b31d
LauraVargas <tinidomin58@gmail.com>
2023-07-24 22:14:32 -0500

    Se agregó JavaScript

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// console.log("Analisis y Desarrollo de Software");
+
+
+var edad=33;
+alert("Mi nombre es: "+edad);
+const edad=22;
+let edad=33;
+
+
+const nom="Laura"
+const ape='Vargas'
+alert("Mi nombre es "+nom+" y mi apellido es "+ape)
+
+
+const num1=2
+const num2=2
+const suma = num1 + num2
+parseInt (suma)
+alert("la suma es: "+suma) 
+
+
+const num1=5
+const num2=2.5
+const resta = num1 - num2
+alert("la resta es: "+resta)
+
+
+const num1=3
+const num2=6
+const multi = num1 * num2
+alert("la multi es: "+multi)
+
+
+
+const num1=100
+const num2=10
+const div = num1 / num2
+alert("la division es: "+div)
+
+
+
+let nom=prompt("Escriba el nombre")
+let ape=prompt("Escriba sus apellidos")
+let edad=prompt("Escriba su edad")
+let peso=prompt("Escriba su peso")
+let dir=prompt("Escriba su dirección")
+alert("Mi nombre es "+nom+", mis apellidos son "+ape+", tengo "+edad+" años, en estos momentos estoy pesando "+peso+" kilos y mi dirección es "+dir)
